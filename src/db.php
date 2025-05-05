454e96e77423c4253f107f2a4315f88df13359e0
marwan-salem18 <marwan.salem209@gmail.com>
2025-05-05 06:57:37 +0300

    added .env config for better use

--- src/db.php
+++ src/db.php
@@ -1,13 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'AMS';
-$user = 'root';
-$pass = 'root';
 
+$env = parse_ini_file(__DIR__.'/../.env');
+$host = $env['host'];
+$dbname = $env['dbname'];
+$username = $env['username'];
+$password = $env['password'];
 try {
-
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
-    
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 } catch (PDOException $e) {
     die("Database error: " . $e->getMessage());
 }
